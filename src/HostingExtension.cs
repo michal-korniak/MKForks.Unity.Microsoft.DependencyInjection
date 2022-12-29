fe46c53241f14243d4512eebfedfdef5ba48d81b
Michal Korniak <michal.korniak@kornit.com>
2022-12-29 11:03:32 +0100

    Added IHostBuilder.UseUnityServiceProvider extension method

--- src/HostingExtension.cs
+++ src/HostingExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace Unity.Microsoft.DependencyInjection
@@ -8,6 +9,19 @@ namespace Unity.Microsoft.DependencyInjection
     public static class HostingExtension
     {
         private static ServiceProviderFactory _factory;
+
+        public static IHostBuilder UseUnityServiceProvider(this IHostBuilder hostBuilder, IUnityContainer container = null, Action<ServiceProviderOptions> options = null)
+        {
+            var optionsObject = ServiceProviderOptions.Create(options);
+            _factory = new ServiceProviderFactory(container, optionsObject);
+
+            return hostBuilder.UseServiceProviderFactory<IUnityContainer>(_factory)
+                              .ConfigureServices((context, services) =>
+                              {
+                                  services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IUnityContainer>>(_factory));
+                                  services.Replace(ServiceDescriptor.Singleton<IServiceProviderFactory<IServiceCollection>>(_factory));
+                              });
+        }
 
         public static IWebHostBuilder UseUnityServiceProvider(this IWebHostBuilder hostBuilder, IUnityContainer container = null, Action<ServiceProviderOptions> options = null)
         {
